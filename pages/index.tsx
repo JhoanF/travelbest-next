997020ec54bf4420299f95a9a63348f379aec1d2
jhoanf <jhoan.o.falcongonzalez@gmail.com>
2022-11-16 20:45:49 -0500

    lint fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="text-3xl flex justify-center align-middle">
       <div className="flex justify-center p-96">
-        The Falcon's
+        The Falcon&apos;s
       </div>
     </div >
   )
